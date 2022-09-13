a1702211d041f96024572df5063a06255cc51ea8
andrewgunyan <59785808+andrewgunyan@users.noreply.github.com>
2022-09-13 16:17:00 -0600

    Update users.js

--- back-end/users.js
+++ back-end/users.js
@@ -8,8 +8,7 @@ const router = express.Router();
 // User schema and model
 //
 
-// This is the schema. Users have usernames and passwords. We solemnly promise to
-// salt and hash the password!
+// This is the user schema.
 const userSchema = new mongoose.Schema({
   firstName: String,
   lastName: String,
@@ -18,17 +17,15 @@ const userSchema = new mongoose.Schema({
   numClicks: Number,
 });
 
-// This is a hook that will be called before a user record is saved,
-// allowing us to be sure to salt and hash the password first.
+// Salt and hash the password first.
 userSchema.pre('save', async function(next) {
   // only hash the password if it has been modified (or is new)
   if (!this.isModified('password'))
     return next();
 
   try {
-    // generate a hash. argon2 does the salting and hashing for us
+    // generate a hash.
     const hash = await argon2.hash(this.password);
-    // override the plaintext password with the hashed one
     this.password = hash;
     next();
   } catch (error) {
@@ -37,14 +34,9 @@ userSchema.pre('save', async function(next) {
   }
 });
 
-// This is a method that we can call on User objects to compare the hash of the
-// password the browser sends with the has of the user's true password stored in
-// the database.
+// Compare hash and password
 userSchema.methods.comparePassword = async function(password) {
   try {
-    // note that we supply the hash stored in the database (first argument) and
-    // the plaintext password. argon2 will do the hashing and salting and
-    // comparison for us.
     const isMatch = await argon2.verify(this.password, password);
     return isMatch;
   } catch (error) {
@@ -52,10 +44,6 @@ userSchema.methods.comparePassword = async function(password) {
   }
 };
 
-// This is a method that will be called automatically any time we convert a user
-// object to JSON. It deletes the password hash from the object. This ensures
-// that we never send password hashes over our API, to avoid giving away
-// anything to an attacker.
 userSchema.methods.toJSON = function() {
   var obj = this.toObject();
   delete obj.password;
@@ -97,15 +85,9 @@ const validUser = async (req, res, next) => {
 
 /* API Endpoints */
 
-/* All of these endpoints start with "/" here, but will be configured by the
-   module that imports this one to use a complete path, such as "/api/user" */
-
 // create a new user
 router.post('/', async (req, res) => {
 console.log("Create User");
-  // Make sure that the form coming from the browser includes all required fields,
-  // otherwise return an error. A 400 error means the request was
-  // malformed.
   if (!req.body.firstName || !req.body.lastName || !req.body.username || !req.body.password)
     return res.status(400).send({
       message: "first name, last name, username and password are required"
@@ -113,8 +95,7 @@ console.log("Create User");
 
   try {
 
-    //  Check to see if username already exists and if not send a 403 error. A 403
-    // error means permission denied.
+    //  Check to see if username already exists and if not send a 403 error.
     const existingUser = await User.findOne({
       username: req.body.username
     });
@@ -165,8 +146,7 @@ console.log("Login User");
         message: "username or password is wrong"
       });
 
-    // Return the SAME error if the password is wrong. This ensure we don't
-    // leak any information about which users exist.
+    // Return the error if the password is wrong.
     if (!await user.comparePassword(req.body.password))
       return res.status(403).send({
         message: "username or password is wrong"
